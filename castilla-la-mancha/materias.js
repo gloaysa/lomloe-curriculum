94a041a43129d8388e3c19f9c9e07197f698f694
d3stroya <destroya@users.noreplay.github.com>
2022-09-05 13:43:01 +0200

    curriculos eso y bach clm

---
+++ castilla-la-mancha/materias.js
@@ -0,0 +1,159 @@
+const eso = {
+    nombre: "Decreto 82/2022, de 12 de julio, por el que se establece la ordenación y el currículo de Educación Secundaria Obligatoria en la comunidad autónoma de Castilla-La Mancha",
+    primero: [
+        "Lengua Castellana y Literatura",
+        "Matemáticas",
+        "Lengua Extranjera",
+        "Geografía e Historia",
+        "Educación Física",
+        "Biología y Geología",
+        "Música",
+        "Tecnología y Digitalización",
+        "Segunda Lengua Extranjera",
+        "Taller de Emprendimiento y finanzas Personales",
+        "Proyectos de Artes Plásticas y Visuales"
+    ],
+    segundo: [
+        "Lengua Castellana y Literatura",
+        "Matemáticas",
+        "Lengua Extranjera",
+        "Geografía e Historia",
+        "Educación Física",
+        "Física y Química",
+        "Música",
+        "Educación Plástica, Visual y Audiovisual",
+        "Educación en Valores Cívicos y Éticos	",
+        "Segunda Lengua Extranjera",
+        "Cultura Clásica",
+        "Desarrollo Digital"
+    ],
+    tercero: [
+        "Lengua Castellana y Literatura",
+        "Matemáticas",
+        "Lengua Extranjera",
+        "Geografía e Historia",
+        "Educación Física",
+        "Biología y Geología",
+        "Física y Química",
+        "Educación Plástica, Visual y Audiovisual",
+        "Tecnología y Digitalización",
+        "Segunda Lengua Extranjera",
+        "Emprendimiento, Sostenibilidad y consumo responsable",
+        "Música Activa, Movimiento y Folclore"
+    ],
+    cuarto: [
+        "Lengua Castellana y Literatura",
+        "Matemáticas A",
+        "Matemáticas B",
+        "Lengua Extranjera",
+        "Geografía e Historia",
+        "Educación Física",
+        "Biología y Geología",
+        "Física y Química",
+        "Digitalización",
+        "Economía y Emprendimiento",
+        "Latín",
+        "Formación y Orientación Personal y Profesional",
+        "Expresión Artística",
+        "Música",
+        "Tecnología",
+        "Segunda Lengua Extranjera",
+        "Filosofía",
+        "Cultura Clásica",
+        "Cultura Científica",
+        "Proyectos de Robótica",
+        "Artes Escénicas, Danza y Folclore"
+    ]
+}
+
+const bach = {
+    primero: [
+        // ciencias y tecnología
+        "Educación Física",
+        "Filosofía",
+        "Lengua Castellana y Literatura I",
+        "Lengua Extranjera I ",
+        "Matemáticas I",
+        "Biología, Geología y Ciencias Ambientales",
+        "Dibujo Técnico I ",
+        "Física y Química ",
+        "Tecnología e Ingeniería I",
+        "2ª Lengua Extranjera I",
+        "Anatomía Aplicada",
+        "Desarrollo Digital",
+
+        // ccss
+        "Latín I ",
+        "Matemáticas aplicadas a las CCSS I ",
+        "Economía",
+        "Griego I",
+        "Historia del Mundo Contemporáneo",
+        "Literatura Universal",       
+        "Lenguaje y Práctica Musical",
+        "Psicología",
+
+        // artes
+        "Dibujo Artístico I ",
+        "Cultura Audiovisual",
+        "Dibujo Técnico Aplicado a Artes Plásticas y  Diseño I",
+        "Proyectos Artísticos",
+        "Volumen",
+
+        // música
+        "Análisis Musical I ",
+        "Artes Escénicas I",
+        "Coro y Técnica Vocal I",
+
+        // general
+        "Matemáticas Generales",
+        "Economía, Emprendimiento y Actividad Empresarial"
+    ],
+    segundo: [
+        // ciencias y tecnología
+        "Historia de España",
+        "Historia de la Filosofía",
+        "Lengua Castellana y Literatura II",
+        "Lengua Extranjera II",
+        "Matemáticas II",
+        "Matemáticas aplicadas a las CCSS II",
+        "Biología",
+        "Dibujo Técnico II",
+        "Física",
+        "Geología y Ciencias Ambientales",
+        "Química",
+        "Tecnología e Ingeniería II",
+        "2ª Lengua Extranjera II",
+        "Investigación y Desarrollo Científico",
+        "Creación de Contenidos Artísticos y Audiovisuales",
+
+        // ccss
+        "Latín II",
+        "Matemáticas aplicadas a las CCSS II",
+        "Empresa y Diseño de Modelos de Negocio",
+        "Geografía",
+        "Griego II",
+        "Historia del Arte",
+        "Historia de la Música y la Danza",
+        "Fundamentos de Administración y Gestión",       
+
+        // artes
+        "Dibujo Artístico II",
+        "Dibujo Técnico Aplicado a Artes Plásticas y  Diseño II",
+        "Diseño",
+        "Fundamentos Artísticos",
+        "Técnicas de Expresión Gráfico-Plástica",
+        "Fundamentos de Administración y Gestión",
+
+        // música
+        "Análisis Musical II",
+        "Artes Escénicas II",
+        "Coro y Técnica Vocal II",
+        "Historia de la Música y de la Danza",
+        "Literatura Dramática",
+        "Proyectos Artísticos de Música, Danza y Arte Dramático",
+
+        // general
+        "Ciencias Generales",
+        "Movimientos Culturales y Artísticos"
+    ]
+}
