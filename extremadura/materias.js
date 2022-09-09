9721d489a5e0a86c0ab0ca683579ded5f55be3df
d3stroya <destroya@users.noreplay.github.com>
2022-09-09 11:18:52 +0200

    progreso en readme

---
+++ extremadura/materias.js
@@ -0,0 +1,157 @@
+const eso = {
+    nombre: "Decreto 82/2022, de 12 de julio, por el que se establece la ordenación y el currículo de Educación Secundaria Obligatoria en la comunidad autónoma de Castilla-La Mancha",
+    primero: [
+        "Biología y Geología",
+        "Educación Física",
+        "Educación Plástica, Visual y Audiovisual",
+        "Geografía e Historia",
+        "Lengua Castellana y Literatura",
+        "Lengua Extranjera",
+        "Matemáticas",
+        "Música",
+        "Segunda Lengua Extranjera",
+        "Digitalización básica"
+    ],
+    segundo: [
+        "Educación Física",
+        "Educación en Valores Cívicos y Éticos	",
+        "Física y Química",
+        "Geografía e Historia",
+        "Lengua Castellana y Literatura",
+        "Lengua Extranjera",
+        "Matemáticas",
+        "Música",
+        "Tecnología y Digitalización",
+        "Segunda Lengua Extranjera",
+        "Emprendimiento Social y Sostenibilidad"
+    ],
+    tercero: [
+        "Educación Física",
+        "Biología y Geología",
+        "Educación Plástica, Visual y Audiovisual",
+        "Física y Química",
+        "Geografía e Historia",
+        "Lengua Castellana y Literatura",
+        "Lengua Extranjera",
+        "Matemáticas",
+        "Tecnología y Digitalización",
+        "Segunda Lengua Extranjera",
+        "Cultura Clásica",
+        "Unión Europea"
+    ],
+    cuarto: [
+        "Educación Física",
+        "Geografía e Historia",
+        "Lengua Castellana y Literatura",
+        "Lengua Extranjera",
+        "Matemáticas A",
+        "Matemáticas B",
+        "Biología y Geología",
+        "Digitalización",
+        "Economía y Emprendimiento",
+        "Expresión Artística",
+        "Física y Química",
+        "Formación y Orientación Personal y Profesional",
+        "Latín",
+        "Música",
+        "Segunda Lengua Extranjera",
+        "Tecnología",
+        "Unión Europea",
+        "Introducción a la filosofía",
+    ]
+}
+
+const bach = {
+    primero: [
+        // comunes
+        "Educación Física",
+        "Filosofía",
+        "Lengua Castellana y Literatura I",
+        "Lengua Extranjera I ",
+
+        // ciencias y tecnología
+        "Matemáticas I",
+        "Biología, Geología y Ciencias Ambientales",
+        "Dibujo Técnico I ",
+        "Física y Química ",
+        "Tecnología e Ingeniería I",
+
+        // ccss
+        "Latín I ",
+        "Matemáticas aplicadas a las CCSS I ",
+        "Economía",
+        "Griego I",
+        "Historia del Mundo Contemporáneo",
+        "Literatura Universal",       
+
+        // artes
+        "Dibujo Artístico I ",
+        "Cultura Audiovisual",
+        "Dibujo Técnico Aplicado a Artes Plásticas y  Diseño I",
+        "Proyectos Artísticos",
+        "Volumen",
+
+        // música
+        "Análisis Musical I ",
+        "Artes Escénicas I",
+        "Coro y Técnica Vocal I",
+        "Lenguaje y Práctica Musical",
+
+        // general
+        "Matemáticas Generales",
+        "Economía, Emprendimiento y Actividad Empresarial",
+
+        // optativas
+        "Segunda Lengua Extranjera",
+        "Inteligencia Artificial"
+    ],
+    segundo: [
+        // comunes
+        "Historia de España",
+        "Historia de la Filosofía",
+        "Lengua Castellana y Literatura II",
+        "Lengua Extranjera II",
+
+        // ciencias y tecnología
+        "Matemáticas II",
+        "Matemáticas aplicadas a las CCSS II",
+        "Biología",
+        "Dibujo Técnico II",
+        "Física",
+        "Geología y Ciencias Ambientales",
+        "Química",
+        "Tecnología e Ingeniería II",
+
+        // ccss
+        "Latín II",
+        "Matemáticas aplicadas a las CCSS II",
+        "Empresa y Diseño de Modelos de Negocio",
+        "Geografía",
+        "Griego II",
+        "Historia del Arte",   
+
+        // artes
+        "Dibujo Artístico II",
+        "Dibujo Técnico Aplicado a Artes Plásticas y  Diseño II",
+        "Diseño",
+        "Fundamentos Artísticos",
+        "Técnicas de Expresión Gráfico-Plástica",
+
+        // música
+        "Análisis Musical II",
+        "Artes Escénicas II",
+        "Coro y Técnica Vocal II",
+        "Historia de la Música y de la Danza",
+        "Literatura Dramática",
+
+        // general
+        "Ciencias Generales",
+        "Movimientos Culturales y Artísticos",
+
+        // optativas
+        "Segunda Lengua Extranjera",
+        "Psicología",
+        "Actividad Física y Salud",
+        "Ecología y Sostenibilidad Ambiental"
+    ]
+}
